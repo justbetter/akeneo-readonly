04512566f74aa5d47c7b44429694536d69ca3889
Vincent Boon <vincent@justbetter.nl>
2021-11-18 10:47:49 +0100

    Initial commit - Add Laravel and Jetstream

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+         User::create([
+             'email' => 'admin@admin.com',
+             'name' => 'Administrator',
+             'password' => bcrypt('VerySecurePassword')
+         ]);
+    }
+}
